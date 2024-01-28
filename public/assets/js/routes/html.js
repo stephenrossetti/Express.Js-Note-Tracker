dd991bd3052f02335b08039ca0d8babe8cfebd88
Stevie Rossetti <rossetti.stephen@gmail.com>
2024-01-27 18:26:19 -0600

    Created html.js to set up get routes to html pages.

---
+++ public/assets/js/routes/html.js
@@ -0,0 +1,18 @@
+// Set up Router //
+// Standard require express and path functionalities //
+const htmlRouter = require('express').Router();
+const path = require('path');
+
+// Utilized Module 11 - Activity 22 for example code on GET functions //
+// Utilize htmlRouter variable (router()) to create GET route for homepage //
+htmlRouter.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+// Create GET route for notes page //
+htmlRouter.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Export htmlRouter if needed //
+module.exports = htmlRouter;
