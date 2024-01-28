c65e807d82d029e524b3e1e5b083381e0be7e16f
Stevie Rossetti <rossetti.stephen@gmail.com>
2024-01-27 20:39:42 -0600

    fixing path mistakes

--- public/assets/js/routes/api.js
+++ public/assets/js/routes/api.js
@@ -1,18 +1,19 @@
 // Create variable for API route and require FS for file system functions (read/write) //
 const apiRouter = require('express').Router();
 const fs = require('fs');
-const uuid = require('../uuid.js');
+const uuid = require('../uuid');
 
 // GET route for retrieving the notes //
 apiRouter.get('/api/notes', (req,res) => {
-    const data = fs.readFileSync('./db/db.json');
+    const data = fs.readFileSync('../../../../db/db.json');
     const db = JSON.parse(data);
     res.json(db);
 });
 
+require('../../../../db/db.json')
 // POST route for new note //
 apiRouter.post('/api/notes', (req, res) => {
-    const data = fs.readFileSync('./db/db.json');
+    const data = fs.readFileSync('../../../../db/db.json');
     const db = JSON.parse(data);
     
     const { title, text } = req.body;
@@ -25,7 +26,7 @@ apiRouter.post('/api/notes', (req, res) => {
       };
   
       db.push(newNote);
-      fs.writeFileSync('./db/db.json',JSON.stringify(db));
+      fs.writeFileSync('../../../../db/db.json',JSON.stringify(db));
       res.json(db);
     } else {
       res.error('Error in adding note');
